1cc0093e0dc9c7128579c908ae1a93e85630187e
supriyashilwant <supriya.shilwant2018@gmail.com>
2023-08-20 06:32:49 +0530

    To show synchronization

---
+++ src/test/java/com/practice/GitPractice.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		System.out.println("For Teesting Purpose only");
+
+	}
+
+}
--- src/test/java/com/practice/SynchronizationInSelenium.java
+++ src/test/java/com/practice/SynchronizationInSelenium.java
@@ -3,7 +3,7 @@ package com.practice;
 public class SynchronizationInSelenium {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println();
 
 	}
 
